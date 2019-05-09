1e7e852074419d27e8770fe21cdfddc9bd7b22df
RomanBogatikov <ivaroman2000@gmail.com>
2019-05-09 14:37:08 -0400

    finished server.js /cars middleware

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const carsController = require('./controllers/cars.js');
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use('/cars', carsController);
+
+
 
 app.listen(PORT, () => {
   console.log('collaborating on port', PORT);
